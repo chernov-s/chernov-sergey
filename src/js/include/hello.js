87c0462d9ff96b173e62b298631f1b459e1c4454
sosed <chernov.net@ya.ru>
2017-05-16 22:17:17 +1000

    Change links to projects. Add yandex counter

--- src/js/include/hello.js
+++ src/js/include/hello.js
@@ -196,7 +196,7 @@ window.requestAnimationFrame = (function(){
             {word: "МЕНЯ ЗОВУТ*ЧЕРНОВ СЕРГЕЙ", time: 2500, effect: 1 },
             {word: "Я ФРОНТЕНД*РАЗРАБОТЧИК", time: 2500, effect: 2 },
             {word: "ЕСТЬ*ИНТЕРЕСНЫЙ*ПРОЕКТ?", time: 3500, effect: 1 },
-            {word: "А ЕСЛИ*НАЙДУ?", time: 3500, effect: 2 }
+            {word: "А ЕСЛИ НАЙДУ?", time: 3500, effect: 2 }
         ],
         mouse = new Vector(),
         isDown = false,
